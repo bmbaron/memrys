f035f1bed184cf33e465debfe1932348535a5efc
bmbaron <benjamin.m.baron@gmail.com>
2024-12-18 17:30:13 +0700

    removed trailing slash from origin for CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3000;
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://memrys.netlify.app/']
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://memrys.netlify.app']
   }),
   express.json()
 );
