1e6b340c6323c5e7873a7c85fba0666a5371619b
bmbaron <benjamin.m.baron@gmail.com>
2024-12-19 14:36:36 +0700

    Fix responsiveness and validation of sign up and register

--- frontend/src/pages/Authentication.tsx
+++ frontend/src/pages/Authentication.tsx
@@ -12,7 +12,7 @@ import {
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { upperFirst, useToggle } from '@mantine/hooks';
-import { FormEvent, useContext, useEffect } from 'react';
+import React, { FormEvent, useContext, useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import classes from '../../src/Authentication.module.css';
 import { getGradientColorText } from '../utils/gradientColorText.tsx';
@@ -39,35 +39,64 @@ const Authentication = () => {
 
   const handleChange = () => {
     setSearchParams(`mode=${formType === 'login' ? 'register' : 'login'}`);
+    if (formType === 'login') {
+      myForm.setValues({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+        terms: false
+      });
+    } else {
+      if (myForm.getValues().testing) {
+        myForm.setValues({
+          email: 'lara@croft.com',
+          password: '12345678'
+        });
+      }
+    }
     toggle();
+  };
+
+  const handleTesting = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.checked) {
+      myForm.setValues({
+        email: 'lara@croft.com',
+        password: '12345678'
+      });
+    } else {
+      myForm.setValues({
+        email: '',
+        password: ''
+      });
+    }
+    myForm.setFieldValue('testing', event.currentTarget.checked);
   };
 
   const myForm = useForm({
     initialValues: {
-      name: 'a',
-      email: 'lara@croft.com',
-      password: '12345678',
-      password2: '111111',
-      terms: true
+      testing: false,
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      terms: false
     },
     validate: {
       email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
       password: (value) =>
         value.length < 6 ? 'Password should include at least 6 characters' : null,
-      password2: (value, values) => (value !== values.password ? 'Passwords did not match' : null),
+      password2: (value, values) => (value !== values.password ? 'Passwords do not match' : null),
       terms: (value) => !value && 'Please accept the terms'
-    }
+    },
+    validateInputOnChange: true
   });
 
   const handleRegister = async () => {
     try {
       const register = await postRegister(myForm.getValues());
       const resString = JSON.stringify(register);
-      if (resString !== '{}') {
-        return true;
-      } else {
-        return false;
-      }
+      return resString !== '{}';
     } catch (err: unknown) {
       console.error((err as Error).message);
       return false;
@@ -115,7 +144,7 @@ const Authentication = () => {
       if (register) {
         await handleSuccess(true);
       } else {
-        alert('there was a problem logging you in');
+        alert('there was a problem registering you');
       }
     } else {
       const login = await handleLogin();
@@ -128,37 +157,48 @@ const Authentication = () => {
   };
   return (
     <div className={classes.wrapper}>
-      <Paper className={classes.form} radius={0} p={30} pt={70}>
+      <Paper className={classes.form} radius={0} p={30} pt={30}>
         <Flex direction={'column'} gap={20}>
           <Flex style={{ fontSize: 40 }}>
             <Title order={1}>let&apos;s make&nbsp;</Title>
             {getGradientColorText('memrys', 'orange', 'cyan', 1)}
           </Flex>
           <form onSubmit={handleSubmit}>
-            <Stack>
+            <Stack mt={20}>
               {formType === 'register' && (
                 <TextInput
                   required
                   label={'Name'}
-                  placeholder={'Your name'}
+                  placeholder={'Lara Croft'}
                   value={myForm.values.name}
                   onChange={(event) => myForm.setFieldValue('name', event.currentTarget.value)}
                   radius={'md'}
+                  styles={{ input: { border: '1px solid var(--mantine-color-gray-4)' } }}
+                />
+              )}
+              {formType === 'login' && (
+                <Checkbox
+                  label={'Use sandbox account'}
+                  checked={myForm.values.testing}
+                  onChange={handleTesting}
+                  error={myForm.errors.testing}
                 />
               )}
               <TextInput
+                type={'email'}
                 required
                 label={'Email'}
-                placeholder={'hello@test.com'}
+                placeholder={'lara@croft.com'}
                 value={myForm.values.email}
                 onChange={(event) => myForm.setFieldValue('email', event.currentTarget.value)}
                 error={myForm.errors.email}
                 radius={'md'}
+                styles={{ input: { border: '1px solid var(--mantine-color-gray-4)' } }}
               />
               <PasswordInput
                 required
                 label={'Password'}
-                placeholder={'Your password'}
+                placeholder={'Must be at least 6 characters'}
                 value={myForm.values.password}
                 onChange={(event) => myForm.setFieldValue('password', event.currentTarget.value)}
                 error={myForm.errors.password}
@@ -169,7 +209,7 @@ const Authentication = () => {
                   <PasswordInput
                     required
                     label={''}
-                    placeholder={'Confirm your password'}
+                    placeholder={'Confirm password'}
                     value={myForm.values.password2}
                     onChange={(event) =>
                       myForm.setFieldValue('password2', event.currentTarget.value)
@@ -178,7 +218,9 @@ const Authentication = () => {
                     radius={'md'}
                   />
                   <Checkbox
-                    label={'I accept terms and conditions'}
+                    label={
+                      'I agree to share my email to facilitate account creation and logging in'
+                    }
                     checked={myForm.values.terms}
                     onChange={(event) => myForm.setFieldValue('terms', event.currentTarget.checked)}
                     error={myForm.errors.terms}
@@ -186,7 +228,7 @@ const Authentication = () => {
                 </>
               )}
             </Stack>
-            <Group justify={'space-between'} mt={'xl'}>
+            <Group justify={'space-between'} mt={60}>
               <Anchor
                 component={'button'}
                 type={'button'}
